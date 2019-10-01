fc0ec723708e12131ec4e8644a6c1a15b6d345bc
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-09-30 20:55:10 -0400

    Created the models

--- server.js
+++ server.js
@@ -25,3 +25,8 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
