84ddc9376704beb3f824ac74925cf605832cca4b
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-10-05 20:45:49 -0400

    Created a route to get an individual article. Added a modal to hold comments.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,30 @@
-// Grab the articles as a json
-/*$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-  }
-});*/
+$(document).on("click", "#leaveNote", function() {
+  // Empty the comment from the comments section
+  $("#yourComment").empty();
+  // Save the id from the p tag
+  var thisId = $(this).attr("data-id");
+
+  // Now make an ajax call for the Article
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  })
+    // With that done, add the note information to the page
+    .then(function(data) {
+      console.log(data);
+      // The title of the article
+      // $("#yourComment").append("<h2>" + data.title + "</h2>");
+      // A textarea to add a new note body
+      // $("#yourComment").append("<textarea id='bodyinput' name='body'></textarea>");
+      // A button to submit a new note, with the id of the article saved to it
+      // $("#yourComment").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+
+      // If there's a comment for the article
+      if (data.note) {
+        // Place the title of the note in the title input
+        $("#titleinput").val(data.note.title);
+        // Place the body of the note in the body textarea
+        $("#bodyinput").val(data.note.body);
+      }
+    });
+});
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -13,14 +13,14 @@ router.get("/", function(req, res){
         var hbsObject = {
             articles: dbArticle
         };
-    //   res.json(dbArticle);
+
     res.render("index", hbsObject);
     })
     .catch(function(err){
       res.json("There was an error retrieving the articles. \n", err);
     });
     
-})
+});
 
 // GET route to scrape Google News
 router.get("/scrape", function(req, res) {
@@ -74,5 +74,38 @@ router.get("/scrape", function(req, res) {
       res.json("There was an error retrieving the articles. \n", err);
     })
   });
+
+  // GET route for one article and populate it's comment
+  router.get("/api/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      // Populate all of the comments associated with the article
+      .populate("comment")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  // Route for saving/updating an Article's associated Note
+/*app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });*/
 
 module.exports = router;
