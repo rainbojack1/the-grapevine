ee6b883e318a7e2878410967ea262caf0244d139
Jesselyn Jackson <rainbojack1@Users.NoReply.Github.com>
2019-10-05 21:32:00 -0400

    Updated post route to include /api in the url. Display existing comments in modal if they exist. Save or update comments in the modal.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,30 +1,43 @@
-$(document).on("click", "#leaveNote", function() {
-  // Empty the comment from the comments section
-  $("#yourComment").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
+var thisId;
 
-  // Now make an ajax call for the Article
+$(document).on("click", "#leaveNote", function() {
+  // Save the id from the p tag
+  thisId = $(this).attr("data-id");
+
+  // Make an ajax call for the Article
   $.ajax({
     method: "GET",
-    url: "/articles/" + thisId
+    url: "/api/articles/" + thisId
   })
-    // With that done, add the note information to the page
     .then(function(data) {
       console.log(data);
-      // The title of the article
-      // $("#yourComment").append("<h2>" + data.title + "</h2>");
-      // A textarea to add a new note body
-      // $("#yourComment").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      // $("#yourComment").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
+      // A textarea to the comment modal
+      $("#yourComment").append("<textarea id='bodyinput' name='body' placeholder='Add a comment'></textarea>");
+      
       // If there's a comment for the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
+      if (data.comment) {
+        $("#bodyinput").val(data.comment.body);
       }
     });
+});
+
+$(document).on("click", "#saveBtn", function() {
+  // Close the modal
+  // $("#comments").hide();
+
+  // Run a POST request to change the note, using what's entered in the inputs
+  $.ajax({
+    method: "POST",
+    url: "/api/articles/" + thisId,
+    data: {
+      body: $("#bodyinput").val()
+    }
+  })
+    .then(function(data) {
+      console.log(data);
+      $("#yourComment").empty();
+    });
+
+  // Empty the textarea
+  $("#bodyinput").val("");
 });
